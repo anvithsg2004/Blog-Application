9798062ac558b1b71cf20c395edbc5fd75c1d3fa
Anvith S G <anvithsg2004@gmail.com>
2025-05-21 08:21:24 +0530

    Changed the backend URL for hitting the API

--- Blog-Backend/src/main/java/com/blog/Blog_Backend/config/SecurityConfig.java
+++ Blog-Backend/src/main/java/com/blog/Blog_Backend/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://aidenblog.netlify.app"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
--- Blog-Frontend/src/components/utils/api.js
+++ Blog-Frontend/src/components/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://blog-backend-2d2g.onrender.com";
 
 // Function to get the Base64-encoded credentials from localStorage
 const getAuthCredentials = () => {
