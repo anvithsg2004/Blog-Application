23e1310c225c413b68af86c0ab8a33d45cecdd37
Anvith S G <anvithsg2004@gmail.com>
2025-08-08 03:13:36 +0530

    Solved some error in frontend and solved @EnableMongoAuditing problem

--- Blog-Backend/src/main/java/com/blog/Blog_Backend/config/AppConfig.java
+++ Blog-Backend/src/main/java/com/blog/Blog_Backend/config/AppConfig.java
@@ -6,12 +6,14 @@ import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableCaching
 @EnableAsync
+@EnableMongoAuditing
 public class AppConfig {
     @Bean
     public CacheManager cacheManager() {
